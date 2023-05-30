b2b83fd3090cac13e5064a879d3b1cb95ada06ea
Prakhar Dubey <prakhardubey2002@gmail.com>
2023-05-30 16:24:21 +0530

    Comment Screen completed

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState,useEffect } from 'react';
+import * as Font from 'expo-font';
+import { createStackNavigator } from '@react-navigation/stack';
+import Comment from './assets/Screens/Comment';
+import { NavigationContainer } from '@react-navigation/native';
+import Sce from './assets/Screens/Sce';
+const Stack = createStackNavigator();
 export default function App() {
 
   const [isFontLoaded, setIsFontLoaded] = useState(false);
@@ -19,10 +25,22 @@ export default function App() {
   if(!isFontLoaded) return null;
 
   if(isFontLoaded) return (
-    <View style={styles.container}>
-      <Text>Jai Shree Ram</Text>
+    <NavigationContainer>
+    {/* <View style={styles.container}> */}
+      {/* <Text>Jai Shree Ram</Text>
+      <Button
+        title="Go to Details"
+        
+        onPress={() => navigation.navigate('Home')}
+      /> */}
+      <Stack.Navigator initialRouteName="Comment">
+      <Stack.Screen name="Comment" component={Comment}  />
+      <Stack.Screen name="Sce" component={Sce}  />
+      
+    </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    {/* </View> */}
+    </NavigationContainer>
   );
 }
 
---
+++ assets/Screens/Sce.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { useState,useEffect } from 'react';
+
+export default function Sce() {
+
+  return (
+    <View style={styles.container}>
+     <Text>Home Screaxasncluni;dnipnkojicsokcijscbien</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
